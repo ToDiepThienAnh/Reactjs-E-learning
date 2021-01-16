eaccef6f06e751c06e36c958f294c580824499f4
todiepthienanh <todiepthienanh@gmail.com>
2021-01-16 23:19:40 +0700

    'Create-project'

---
+++ e-learing/src/redux/configStore.js
@@ -0,0 +1,10 @@
+import { combineReducers, createStore } from 'redux';
+
+const rootReducer = combineReducers({
+    // khai báo reducer tại đây
+})
+
+
+const store = createStore(rootReducer);
+
+export default store;
